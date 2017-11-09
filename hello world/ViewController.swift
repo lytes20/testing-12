73dc386fc991672d8db9e46d79e5741952bb778a
gideon bamuleseyo <gideon@MacBooks-MacBook-Pro.local>
2017-11-09 05:52:58 +0300

    initial commit

---
+++ hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by gideon bamuleseyo on 09/11/2017.
+//  Copyright © 2017 gideon bamuleseyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
